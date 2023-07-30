d1d83f59a25cfff7a215b7f65f49bcbfa0481cef
Peter R <prodger2@gmail.com>
2023-07-30 16:58:15 +0200

    First commit. Or second. Can't remember

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,28 @@
-function HomePage() {}
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+function HomePage() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const countryDetail = async () => {
+      const response = await axios.get(
+        "https://ih-countries-api.herokuapp.com/countries"
+      );
+      const countriesData = response.data;
+      setCountries(countriesData);
+    };
+    countryDetail();
+  }, []);
+
+  return (
+    <>
+      <h1>WikiCountries: Your Guide to the World</h1>;
+      {countries.map((aCountry) => {
+          return <p key={aCountry._id}>{aCountry.name.common}</p>;
+          
+      })}
+    </>
+  );
+}
 export default HomePage;
